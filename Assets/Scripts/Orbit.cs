d1f3300e1bd5fb8405da1622cd2828c00313eafa
DC <dcdolphy@gmail.com>
2018-02-15 18:41:53 +0000

    Added basic solar system with orbiting bodies.

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject target;
+    public float orbitSpeed;
+
+    private Transform targetPosition;
+
+    private void Start()
+    {
+        // Get the target object's transform
+        targetPosition = target.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        // Rotate the object around the central object
+        transform.RotateAround(targetPosition.position, Vector3.up, orbitSpeed * Time.deltaTime);
+    }
+}
