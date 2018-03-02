be4e47804e218c9a66b81c18afe24a785f8798ad
DC <dcdolphy@gmail.com>
2018-03-02 13:31:54 +0000

    Added post-processing stack. Fixed planet positions.

--- Assets/Scripts/SolarSystemGenerator.cs
+++ Assets/Scripts/SolarSystemGenerator.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class SolarSystemGenerator : MonoBehaviour
 {
     public Material starMaterial;
     public Material defaultMat;
+    public Material planetMat;
     private Color starColor;
     private GameObject star;
     private GameObject[] planets;
@@ -11,6 +14,8 @@ public class SolarSystemGenerator : MonoBehaviour
     private GameObject colonyMoon;
     private int moonColony = -1;
     private LayerMask mask;
+
+    private List<GameObject> planetList;
 
 	private void Start ()
     {
@@ -32,26 +37,41 @@ public class SolarSystemGenerator : MonoBehaviour
     private void GeneratePlanets()
     {
         int planetAmount = Random.Range(5, 11);
-        planets = new GameObject[planetAmount];
+        //planets = new GameObject[planetAmount];
+        planetList = new List<GameObject>();
         for (int i = 0; i < planetAmount; i++)
         {
             float scaleAmount = Random.Range(500.0f, 10000.0f);
-            planets[i] = CreateEntity("planet " + i, null, ((GameObject)Resources.Load("LessNuttySphere")).GetComponent<MeshFilter>().sharedMesh, defaultMat);
-            planets[i].AddComponent<SphereCollider>();
-            planets[i].AddComponent<Orbit>();
-            planets[i].GetComponent<Orbit>().target = star;
-            planets[i].GetComponent<Orbit>().orbitAngle = new Vector3(Random.Range(0, 45), Random.Range(0, 45), Random.Range(0, 45));
-            planets[i].transform.position = new Vector3(Random.Range(-1000.0f, 1000.0f), Random.Range(-100.0f, 100.0f), Random.Range(-1000.0f, 1000.0f));
-            planets[i].transform.localScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+            //planets[i] = CreateEntity("planet " + i, null, ((GameObject)Resources.Load("LessNuttySphere")).GetComponent<MeshFilter>().sharedMesh, defaultMat);
+            //planets[i].AddComponent<SphereCollider>();
+            //planets[i].AddComponent<Orbit>();
+            //planets[i].GetComponent<Orbit>().target = star;
+            //planets[i].GetComponent<Orbit>().orbitAngle = Vector3.up; //new Vector3(Random.Range(0, 45), Random.Range(0, 45), Random.Range(0, 45));
+            //planets[i].transform.position = new Vector3(Random.Range(-1000.0f, 1000.0f), 0.0f, Random.Range(planets[i].transform.position.z * 2.0f, planets[i].transform.position.z * 12.0f));
+            //planets[i].transform.localScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
 
-            float distance = Vector3.Distance(planets[i].transform.position, star.transform.position);
-            Debug.Log("planet " + i + " distance " + distance);
-            float orbitSpeed = (1 / distance) * 5000;
-            planets[i].GetComponent<Orbit>().orbitSpeed = orbitSpeed;
+            planetList.Add(CreateEntity("planet " + i, null, ((GameObject)Resources.Load("LessNuttySphere")).GetComponent<MeshFilter>().sharedMesh, planetMat));
+            planetList[i].AddComponent<SphereCollider>();
+            planetList[i].AddComponent<Orbit>();
+            planetList[i].GetComponent<Orbit>().target = star;
+            planetList[i].GetComponent<Orbit>().orbitAngle = Vector3.up; //new Vector3(Random.Range(0, 45), Random.Range(0, 45), Random.Range(0, 45));
 
-            Collider[] hits = Physics.OverlapSphere(planets[i].transform.position, planets[i].transform.localScale.x * 1.5f);
+            if(i == 0)
+            {
+                planetList[i].transform.position = new Vector3(Random.Range(-1000.0f, 1000.0f), 0.0f, Random.Range(0.0f, 1000.0f));
+            }
+            if(i >= 1)
+            {
+                planetList[i].transform.position = new Vector3(Random.Range(-1000.0f, 1000.0f), 0.0f, Random.Range(planetList[i - 1].transform.position.z + 100.0f, planetList[i - 1].transform.position.z + 1000.0f));
+            }
+            planetList[i].transform.localScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
 
-            Debug.DrawRay(planets[i].transform.position, Vector3.forward, Color.green);
+            float distance = Vector3.Distance(planetList[i].transform.position, star.transform.position);
+            // Debug.Log("planet " + i + " distance " + distance);
+            float orbitSpeed = (1f / distance) * 5000;
+            planetList[i].GetComponent<Orbit>().orbitSpeed = orbitSpeed;
+
+            //Collider[] hits = Physics.OverlapSphere(planets[i].transform.position, planets[i].transform.localScale.x * 1.5f);
         }
     }
 
@@ -61,7 +81,7 @@ public class SolarSystemGenerator : MonoBehaviour
         moons = new GameObject[moonAmount];
         for(int i = 0; i < moonAmount; i++)
         {
-            GameObject planetTarget = planets[Random.Range(0, planets.Length)];
+            GameObject planetTarget = planetList[Random.Range(0, planetList.Count)];
             float orbitSpeed = Random.Range(planetTarget.GetComponent<Orbit>().orbitSpeed * 1.2f, planetTarget.GetComponent<Orbit>().orbitSpeed * 4f);
             float scaleAmount = Random.Range(0.05f, 0.5f);
             moons[i] = CreateEntity("moon " + i, null, ((GameObject)Resources.Load("LessNuttySphere")).GetComponent<MeshFilter>().sharedMesh, defaultMat);
@@ -80,7 +100,7 @@ public class SolarSystemGenerator : MonoBehaviour
         float scaleAmount = Random.Range(1000.0f, 5000.0f);
         float starStrength = Random.Range(0.1f, 10.0f);
 
-        starMaterial.color = new Color32((byte)Random.Range(50, 255), (byte)Random.Range(50, 255), (byte)Random.Range(50, 255), 255);
+        starMaterial.color = new Color32((byte)Random.Range(75, 255), (byte)Random.Range(75, 255), (byte)Random.Range(75, 255), 255);
         starMaterial.EnableKeyword("_EMISSION");
         starMaterial.SetColor("_EmissionColor", starMaterial.color);
 
@@ -89,7 +109,7 @@ public class SolarSystemGenerator : MonoBehaviour
         star.transform.localScale = scale;
         star.AddComponent<Light>();
         star.GetComponent<Light>().type = LightType.Point;
-        star.GetComponent<Light>().range = scaleAmount * starStrength;
+        star.GetComponent<Light>().range = scaleAmount * starStrength * 4;
         star.GetComponent<Light>().color = starMaterial.color;
     }
 
