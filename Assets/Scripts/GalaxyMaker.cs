8ca7a25d09332787fcd8494577507400c58d04a6
Daniel Cumbor <dcdolphy@gmail.com>
2018-04-15 15:29:40 +0100

    Adde procedural galaxy maker, randomised colours. Radomised positions.

---
+++ Assets/Scripts/GalaxyMaker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyMaker : MonoBehaviour {
+
+    public GameObject spherePrefab;
+
+	// Use this for initialization
+	void Start () {
+        GenerateSpiralGalaxy();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void GenerateSpiralGalaxy()
+    {
+        int spirals = 6;
+        GameObject star;
+
+        // Equation coefficients
+        float a = Random.Range(40, 81); // Global size of galaxy
+        float b = Random.Range(11.12f, 12.42f); // Bulge-to-arm, bigger value = bigger sweep
+        float n = Random.Range(0.706f, 0.902f); // Spiral tightness
+        float brightnessDistance = Random.Range(a / 1.2f, a / 2.6f);
+        transform.eulerAngles = new Vector3(Random.Range(0, 361), Random.Range(0, 361), Random.Range(0, 361));
+
+        for (int i = 0; i < 360 * spirals; i++)
+        {
+            float angle = i * Mathf.Deg2Rad;
+            float distance = a / Mathf.Log10(b * Mathf.Tan(angle / (2 * n)));
+            float angularOffset = Random.Range(-10, 10) * Mathf.Deg2Rad;
+
+            if (distance < Mathf.Abs(a))
+            {
+                float x = Mathf.Cos(angle + angularOffset) * distance;
+                float z = Mathf.Sin(angle + angularOffset) * distance;
+
+                Vector3 finalPosition = transform.position + new Vector3(x, 0, z);
+
+                float starDistance = Vector3.Distance(finalPosition, transform.position);
+                star = Instantiate(spherePrefab, finalPosition, Quaternion.identity, transform);
+
+                if (starDistance > brightnessDistance)
+                {
+                    star.GetComponent<Renderer>().material.color = new Color32((byte)Random.Range(50, 150), (byte)Random.Range(50, 150), (byte)Random.Range(50, 150), 255);
+                    star.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                    star.GetComponent<Renderer>().material.SetColor("_EmissionColor", star.GetComponent<Renderer>().material.color);
+                } 
+                else if (starDistance < brightnessDistance)
+                {
+                    star.GetComponent<Renderer>().material.color = new Color32((byte)Random.Range(125, 255), (byte)Random.Range(125, 255), (byte)Random.Range(125, 255), 255);
+                    star.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+                    star.GetComponent<Renderer>().material.SetColor("_EmissionColor", star.GetComponent<Renderer>().material.color);
+                }
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SolarSystemScript.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemScript : MonoBehaviour
+{
+
+    public GameObject galaxyPrefab;
+
+
+    public GameObject[] galaxyStars;
+    public GameObject spherePrefab;
+
+    public GameObject star;
+    public GameObject planetPrefab;
+    public GameObject[] planets;
+    public Material starMaterial;
+    public int planetCount;
+
+
+	void Start ()
+    {
+        CreateStar();
+        CreatePlanets();
+        GenerateDistantGalaxies();
+        //GenerateSpiralGalaxy();
+
+    }
+
+	void Update ()
+    {
+		
+	}
+
+    private void CreateStar()
+    {
+        float scaleAmount = Random.Range(4.0f, 20.0f);
+        float starStrength = Random.Range(0.1f, 10.0f);
+
+        // If the star is small, we use a cooler colour set. If the star is large, we use hotter colours like with red giants
+        // The planets size also affects how many planets it supports
+        if (scaleAmount < 10.0f)
+        {
+            Debug.Log("Small Star!");
+            starMaterial.color = new Color32((byte)Random.Range(10, 150), (byte)Random.Range(150, 255), (byte)Random.Range(150, 255), 255);
+            starMaterial.EnableKeyword("_EMISSION");
+            starMaterial.SetColor("_EmissionColor", starMaterial.color);
+            planetCount = Random.Range(4, 11);
+        } else if (scaleAmount > 10.0f)
+        {
+            Debug.Log("Big Star!");
+            starMaterial.color = new Color32((byte)Random.Range(150, 255), (byte)Random.Range(50, 200), (byte)Random.Range(25, 100), 255);
+            starMaterial.EnableKeyword("_EMISSION");
+            starMaterial.SetColor("_EmissionColor", starMaterial.color);
+            planetCount = Random.Range(11, 21);
+        }
+        planets = new GameObject[planetCount];
+
+        Vector3 scale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+        star = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere), transform.position, Quaternion.identity);
+        star.GetComponent<Renderer>().material = starMaterial;
+        star.name = "star";
+        star.transform.localScale = scale;
+        star.AddComponent<Light>();
+        star.GetComponent<Light>().type = LightType.Point;
+        star.GetComponent<Light>().range = scaleAmount * starStrength * 2;
+        star.GetComponent<Light>().color = starMaterial.color;
+    }
+
+    private void CreatePlanets()
+    {
+        Vector3[] positions = new Vector3[planetCount];
+        for (int i = 0; i < planetCount; i++)
+        {
+            // Use a random angle and distance from star to place the planets in the solar system
+            float angle = Random.Range(0, 361.0f);
+            float distance = Random.Range(5.0f, 31.0f);
+
+            // Get the position
+            float x = Mathf.Cos(angle * Mathf.Deg2Rad) * distance;
+            float z = Mathf.Sin(angle * Mathf.Deg2Rad) * distance;
+
+            positions[i] = new Vector3(x, 0, z);
+            planets[i] = Instantiate(planetPrefab, positions[i], Quaternion.identity);
+
+            if (i != 0)
+            {
+                float distanceFromOthers = Vector3.Distance(positions[i], positions[i - 1]);
+                Debug.Log("Distance: " + distanceFromOthers);
+
+                if (distanceFromOthers < 10.0f)
+                {
+                   // Destroy(planets[i]);
+                   // planetCount++;
+                    Debug.Log("Destroyed Planet!");
+                }
+            }
+            //Debug.Log(positions[i]);
+        }
+    }
+
+    private void GenerateAsteroidRing()
+    {
+        int starCount = Random.Range(80, 201);
+        galaxyStars = new GameObject[starCount];
+        for (int i = 0; i < starCount; i++)
+        {
+            // Use a random angle and distance from star to place the planets in the solar system
+            float angle = Random.Range(0, 361.0f);
+            float distance = Random.Range(10.0f, 21.0f);
+
+            // Get the position
+            float x = Mathf.Cos(angle * Mathf.Deg2Rad) * distance;
+            float z = Mathf.Sin(angle * Mathf.Deg2Rad) * distance;
+
+            Vector3 finalPosition = transform.position + new Vector3(x, 0, z);
+
+            Instantiate(spherePrefab, finalPosition, Quaternion.identity, transform);
+        }
+    }
+
+    private void GenerateDistantGalaxies()
+    {
+        GameObject galaxy;
+        for (int i = 0; i < 15; i++)
+        {
+            Vector3 position = Random.onUnitSphere * (GetComponent<SphereCollider>().radius) + Vector3.zero;
+            galaxy = Instantiate(galaxyPrefab, position, Quaternion.identity);
+            galaxy.transform.eulerAngles = new Vector3(Random.Range(0, 361), Random.Range(0, 361), Random.Range(0, 361));
+        }
+    }
+}
